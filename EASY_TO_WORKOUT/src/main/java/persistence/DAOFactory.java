df0de06f8e2feb8fcfe08db84b1bb6c015ed89e6
HyunaLee <apg01498@gmail.com>
2021-11-13 22:00:37 +0900

    DAOFactory 관계테이블 제외 주석 제거

--- EASY_TO_WORKOUT/src/main/java/persistence/DAOFactory.java
+++ EASY_TO_WORKOUT/src/main/java/persistence/DAOFactory.java
@@ -9,34 +9,34 @@ public class DAOFactory {
 //	public ChoiceDAO getChoiceDAO() {
 //		return new ChoiceDAOImpl();
 //	}
-//	
-//	public ClubDAO getClubDAO() {
-//		return new ClubDAOImpl();
-//	}
-//	
-//	public ClubScheduleDAO getClubScheduleDAO() {
-//		return new ClubScheduleDAOImpl();
-//	}
-//	
-//	public DiaryDAO getDiaryDAO() {
-//		return new DiaryDAOImpl();
-//	}
-//	
-//	public ExerciseDAO getExerciseDAO() {
-//		return new ExerciseDAOImpl();
-//	}
-//	
-//	public MemberDAO getMemberDAO() {
-//		return new MemberDAOImpl();
-//	}
-//	
+	
+	public ClubDAO getClubDAO() {
+		return new ClubDAOImpl();
+	}
+	
+	public ClubScheduleDAO getClubScheduleDAO() {
+		return new ClubScheduleDAOImpl();
+	}
+	
+	public DiaryDAO getDiaryDAO() {
+		return new DiaryDAOImpl();
+	}
+	
+	public ExerciseDAO getExerciseDAO() {
+		return new ExerciseDAOImpl();
+	}
+	
+	public MemberDAO getMemberDAO() {
+		return new MemberDAOImpl();
+	}
+	
 //	public MembershipDAO getMembershipDAO() {
 //		return new MembershipDAOImpl();
 //	}
 //	
-//	public RoutineDAO getRoutineDAO() {
-//		return new RoutineDAOImpl();
-//	}
+	public RoutineDAO getRoutineDAO() {
+		return new RoutineDAOImpl();
+	}
 //	
 //	public UsageDAO getUsageDAO() {
 //		return new UsageDAOImpl();
