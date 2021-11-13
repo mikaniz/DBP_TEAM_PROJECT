74dff03ba0721f4b4175f06440de96a085117262
user <user@DESKTOP-4OALGTT>
2021-11-13 21:19:17 +0900

    2. Exercise DAO&DTO

--- EASY_TO_WORKOUT/src/main/java/persistence/dao/ExerciseDAO.java
+++ EASY_TO_WORKOUT/src/main/java/persistence/dao/ExerciseDAO.java
@@ -1,5 +1,10 @@
 package persistence.dao;
 
-public class ExerciseDAO {
+import java.util.List;
+import service.dto.ExerciseDTO;
 
+public interface ExerciseDAO {
+
+	public List<ExerciseDTO> getExerciseList();	//운동리스트 반환
+	public ExerciseDTO getExerciseByName(String name);	//운동정보를 운동명으로 찾음
 }
--- EASY_TO_WORKOUT/src/main/java/service/dto/ExerciseDTO.java
+++ EASY_TO_WORKOUT/src/main/java/service/dto/ExerciseDTO.java
@@ -1,5 +1,40 @@
 package service.dto;
 
 public class ExerciseDTO {
-
+	private int exerciseId = 0;
+	private String name = null;
+	private String part = null;
+	private String method = null;
+	
+	public int getExerciseId() {
+		return exerciseId;
+	}
+	
+	public void setExerciseId(int exerciseId) {
+		this.exerciseId = exerciseId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPart() {
+		return part;
+	}
+	
+	public void setPart(String part) {
+		this.part = part;
+	}
+	
+	public String getMethod() {
+		return method;
+	}
+	
+	public void setMethod(String method) {
+		this.method = method;
+	}
 }
