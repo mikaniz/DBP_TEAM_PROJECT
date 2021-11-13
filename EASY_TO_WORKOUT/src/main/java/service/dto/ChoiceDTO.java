8430ad17a6fcfd9657638447ad7c76779d5a46c3
user <user@DESKTOP-4OALGTT>
2021-11-13 21:19:41 +0900

    3.Chocie DAO&DTO

--- EASY_TO_WORKOUT/src/main/java/service/dto/ChoiceDTO.java
+++ EASY_TO_WORKOUT/src/main/java/service/dto/ChoiceDTO.java
@@ -1,5 +1,40 @@
 package service.dto;
 
 public class ChoiceDTO {
-
+	private int exerciseId = 0;
+	private int routineId = 0;
+	private int sequence = 0;
+	private int repetition = 0;
+	
+	public int getExerciseId() {
+		return exerciseId;
+	}
+	
+	public void setExerciseId(int exerciseId) {
+		this.exerciseId = exerciseId;
+	}
+	
+	public int getRoutineId() {
+		return routineId;
+	}
+	
+	public void setRoutineId(int routineId) {
+		this.routineId = routineId;
+	}
+	
+	public int getSequence() {
+		return sequence;
+	}
+	
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+	
+	public int getRepetition() {
+		return repetition;
+	}
+	
+	public void setRepetition(int repetition) {
+		this.repetition = repetition;
+	}
 }
