64fff23e7e67203934a47fd831af97fbb9bfb5e4
user <user@DESKTOP-4OALGTT>
2021-11-16 16:17:59 +0900

    2. 컨트롤러

--- EASY_TO_WORKOUT/src/main/java/controller/routine/ListRoutineController.java
+++ EASY_TO_WORKOUT/src/main/java/controller/routine/ListRoutineController.java
@@ -1,16 +1,48 @@
 package controller.routine;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import service.dto.RoutineDTO;
+import service.RoutineServiceImpl;
 
 public class ListRoutineController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		RoutineServiceImpl manager = RoutineServiceImpl.getInstance();
+		
+		if (request.getMethod().equals("GET")) {
+			List<RoutineDTO> routineList = manager.ListingRoutines();
+			request.setAttribute("routineList", routineList);
+			
+			return "/routine/routinePage.jsp";
+		}
+		
+		String type = request.getParameter("sortRoutine");
+		
+		if (type.equals("1")) {
+			request.setAttribute("checkedOne", true);
+			
+			List<RoutineDTO> routineList = manager.ListingRoutines();
+			request.setAttribute("routineList", routineList);
+		}
+		else if (type.equals("2")) {
+			request.setAttribute("checkedTwo", true);
+			
+			List<RoutineDTO> routineList = manager.ListingRoutinesByPublic();
+			request.setAttribute("routineList", routineList);
+		}
+		else if (type.equals("3")) {
+			request.setAttribute("checkedThree", true);
+			
+			List<RoutineDTO> routineList = manager.ListingRoutinesByPersonal();
+			request.setAttribute("routineList", routineList);
+		}
+		
+		return "/routine/routinePage.jsp";
 	}
 
 }
