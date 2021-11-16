b1d5f32bb927763eb681c0d6706b4812d68694c5
user <user@DESKTOP-4OALGTT>
2021-11-16 16:16:56 +0900

    1. Routine Exception

---
+++ EASY_TO_WORKOUT/src/main/java/service/exception/ExistingRoutineException.java
@@ -0,0 +1,14 @@
+package service.exception;
+
+public class ExistingRoutineException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ExistingRoutineException() {
+		super();
+	}
+	
+	public ExistingRoutineException(String arg0) {
+		super(arg0);
+	}
+}
