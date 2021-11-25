7e07fffb576ac8188ba8e4f2065d694f8f9d391d
user <user@DESKTOP-4OALGTT>
2021-11-25 15:18:58 +0900

    RoutineManager 변수명 수정

--- EASY_TO_WORKOUT/src/main/java/controller/routine/CreateRoutineController.java
+++ EASY_TO_WORKOUT/src/main/java/controller/routine/CreateRoutineController.java
@@ -4,13 +4,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import service.dto.Routine;
+import service.RoutineManager;
 
 public class CreateRoutineController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Routine routine = new Routine();
+		routine.setrName(request.getParameter("routineName"));
+		routine.setRoutineCreater(request.getParameter("routineCreater"));
+		routine.setPart(request.getParameter("routinePart"));
+		routine.setrTime(Integer.parseInt(request.getParameter("routineTime"))); 
+		routine.setDifficulty(Integer.parseInt(request.getParameter("routineLevel"))); 
+		routine.setrType(request.getParameter("routineType"));
+		
+		try {
+			RoutineManager manager = RoutineManager.getInstance();
+			manager.insertRoutine(routine);
+			
+			return "redirect:/routine/list";
+		}catch (Exception e) {
+			request.setAttribute("creationFailed", true);
+			request.setAttribute("exception", e);
+			request.setAttribute("routine", routine);
+			return "/routine/routine_createForm.jsp";
+		}
 	}
 
 }
