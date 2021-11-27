4f1cc5573feb209626b2069e76e49fcacb53db5d
HyunaLee <apg01498@gmail.com>
2021-11-27 20:56:21 +0900

    모임 가입, 탈퇴 구현

--- EASY_TO_WORKOUT/src/main/java/controller/club/DetailClubController.java
+++ EASY_TO_WORKOUT/src/main/java/controller/club/DetailClubController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import controller.Controller;
 import controller.member.MemberSessionUtils;
 import service.ClubManager;
+import service.MembershipManager;
 import service.dto.Club;
 
 public class DetailClubController implements Controller {
@@ -19,28 +20,40 @@ public class DetailClubController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
-		String id = MemberSessionUtils.getLoginMemberId(session);
+		String memberId = MemberSessionUtils.getLoginMemberId(session);
 		
-		ClubManager manager = ClubManager.getInstance();
+		ClubManager clubManager = ClubManager.getInstance();
+		MembershipManager membershipManager = MembershipManager.getInstance();
 		
 		log.debug("detail club : {}", request.getParameter("clubId"));
 		
 		int clubId = Integer.parseInt(request.getParameter("clubId"));
-		Club club = manager.getClubById(clubId);
+		Club club = clubManager.getClubById(clubId);
 		request.setAttribute("club", club);
 		
 		if (request.getParameter("thisIsForDel") != null) {		// 모임 삭제
 			log.debug("delete club : {}", clubId);
-			manager.deleteClub(clubId);
+			clubManager.deleteClub(clubId);
 			
 			return "redirect:/club/list";
 		}
 		
-		if (id.equals(club.getClubMaster())) {	// 모임 개설자 판별
-			request.setAttribute("isMaster", "1");
+		if (request.getParameter("thisIsForOut") != null) {		// 모임 탈퇴
+			log.debug("leave club : {}", clubId);
+			membershipManager.deleteMembership(clubId, memberId);
+			
+			return "redirect:/club/list";
 		}
-		else 
+		
+		if (memberId.equals(club.getClubMaster())) {					// 모임 개설자 판별
+			request.setAttribute("isMaster", "1");
+		} else 
 			request.setAttribute("isMaster", "0");
+		
+		if (membershipManager.isInClub(clubId, memberId)) {			// 해당 모임에 속하는 지 판별 
+			request.setAttribute("isInClub", "1");
+		} else 
+			request.setAttribute("isInClub", "0");
 		
 		return "/club/club_detail.jsp";
 	}
--- EASY_TO_WORKOUT/src/main/java/service/MembershipManager.java
+++ EASY_TO_WORKOUT/src/main/java/service/MembershipManager.java
@@ -1,5 +1,43 @@
 package service;
 
-public class MembershipManager {
+import java.sql.SQLException;
+import java.util.List;
 
+import persistence.DAOFactory;
+import persistence.dao.MembershipDAO;
+import service.dto.Membership;
+import service.exception.AlreadyInClubException;
+
+public class MembershipManager {
+	
+	private static MembershipManager membershipManager = new MembershipManager();
+	private MembershipDAO membershipDao;
+	
+	private MembershipManager() {
+		DAOFactory factory = new DAOFactory();
+		membershipDao = factory.getMembershipDAO();
+	}
+	
+	public static MembershipManager getInstance() {return membershipManager;}
+	
+	public List<Membership> getClubListByMemberId(String memberId) {
+		return membershipDao.getClubListByMemberId(memberId);
+	}
+	
+	public int insertMembership(Membership membership) throws SQLException, AlreadyInClubException {
+		// 이미 가입되어 있으면 exception 발생
+		if (isInClub(membership.getClubId(), membership.getMemberId())) {
+			throw new AlreadyInClubException("이미 해당 모임에 가입되어 있습니다.");
+		}
+		
+		return membershipDao.insertMembership(membership);
+	}
+	
+	public int deleteMembership(int clubId, String memberId) {
+		return membershipDao.deleteMembership(clubId, memberId);
+	}
+	
+	public boolean isInClub(int clubId, String memberId) {
+		return membershipDao.isInClub(clubId, memberId);
+	}
 }
