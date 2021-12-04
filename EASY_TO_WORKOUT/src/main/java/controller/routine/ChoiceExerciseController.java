f41e137db69c689bfa823240fd62b80e460bd5be
user <user@DESKTOP-4OALGTT>
2021-12-05 01:24:58 +0900

    Choice dto, manager, dao, controller 수정

--- EASY_TO_WORKOUT/src/main/java/controller/routine/ChoiceExerciseController.java
+++ EASY_TO_WORKOUT/src/main/java/controller/routine/ChoiceExerciseController.java
@@ -1,5 +1,8 @@
 package controller.routine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -7,8 +10,11 @@ import javax.servlet.http.HttpSession;
 import controller.Controller;
 import controller.member.MemberSessionUtils;
 import service.ExerciseManager;
+import service.MemberManager;
+import service.RoutineManager;
 import service.dto.Exercise;
-
+import service.dto.Member;
+import service.dto.Routine;
 public class ChoiceExerciseController implements Controller {
 	
 	@Override
@@ -20,15 +26,61 @@ public class ChoiceExerciseController implements Controller {
 		}
 		
 		MemberSessionUtils.setLoginUserInfo(session, request);
-		request.setAttribute("btnType", "routineCreate");
 		
-		ExerciseManager manager = ExerciseManager.getInstance();
+		String memberId = MemberSessionUtils.getLoginMemberId(session);
 		
-		int exerciseId = Integer.parseInt(request.getParameter("exerciseId"));
-		Exercise exercise = manager.getExercise(exerciseId);
-		request.setAttribute("exercise", exercise);
+		MemberManager memberManager = MemberManager.getInstance();
+		Member member = memberManager.findMember(memberId);
+		request.setAttribute("member", member);
 		
-		return "/routine/exercise_detail.jsp";
+		if (request.getParameter("checkExerciseId") == null) {
+			request.setAttribute("exerciseNotSelected", "exerciseNotSelected");
+		}
+//		else {
+			request.setAttribute("routineName", request.getParameter("routineName"));
+			request.setAttribute("routinePart", request.getParameterValues("routinePart"));
+			request.setAttribute("routineTime", request.getParameter("routineTime"));
+			request.setAttribute("routineLevel", request.getParameter("routineLevel"));
+			request.setAttribute("routineType", request.getParameter("routineType"));
+			
+			String[] exerciseIdList = request.getParameterValues("checkExerciseId");
+			request.setAttribute("exerciseIdList", exerciseIdList);
+			
+			String[] sequenceList = request.getParameterValues("sequence");
+			request.setAttribute("sequenceList", sequenceList);
+			
+			String[] repetitionList = request.getParameterValues("repetition");
+			request.setAttribute("repetitionList", repetitionList);
+			
+			
+			ExerciseManager exerciseManager = ExerciseManager.getInstance();
+			List<Exercise> exerciseList = new ArrayList<Exercise>();
+			
+			for (String exerciseId : exerciseIdList)  {
+				Exercise exercise = exerciseManager.getExercise(Integer.parseInt(exerciseId));
+				exerciseList.add(exercise);
+			}
+			request.setAttribute("exerciseList", exerciseList);
+			
+//			if (request.getParameter("thisIsForChoice") == null) {
+//				RoutineManager routineManager = RoutineManager.getInstance();
+//				int routineId = Integer.parseInt(request.getParameter("routineId"));
+//				Routine routine = routineManager.getRoutineById(routineId);
+//				request.setAttribute("routine", routine);
+//				
+//				return "/routine/routine_updateForm.jsp";
+//			}
+//		}
+		if (request.getParameter("thisIsForChoice") == null) {
+			RoutineManager routineManager = RoutineManager.getInstance();
+			int routineId = Integer.parseInt(request.getParameter("routineId"));
+			Routine routine = routineManager.getRoutineById(routineId);
+			request.setAttribute("routine", routine);
+			
+			return "/routine/routine_updateForm.jsp";
+		}
+				
+		return "/routine/routine_createForm.jsp";
 	}
 
 }
--- EASY_TO_WORKOUT/src/main/java/persistence/dao/ChoiceDAO.java
+++ EASY_TO_WORKOUT/src/main/java/persistence/dao/ChoiceDAO.java
@@ -1,5 +1,141 @@
 package persistence.dao;
 
-public class ChoiceDAO {
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+import persistence.util.*;
+import service.dto.Choice;
 
+public class ChoiceDAO {
+private JDBCUtil jdbcUtil = null;
+	
+	public ChoiceDAO() {
+		jdbcUtil = new JDBCUtil();
+	}
+
+	public List<Choice> getChoiceList() {
+		String allQuery = "SELECT * FROM choice";
+		jdbcUtil.setSqlAndParameters(allQuery, null);
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();
+			List<Choice> list = new ArrayList<Choice>();
+			
+			while (rs.next()) {
+				Choice dto = new Choice();
+				dto.setRoutineId(rs.getInt("routineId"));
+				dto.setExerciseId(rs.getInt("exerciseId"));
+				dto.setSequence(rs.getInt("sequence"));
+				dto.setRepetition(rs.getInt("repetition"));
+				
+				list.add(dto);
+			}
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return null;
+	}
+
+	public Choice getChoiceListByRoutineId(int routineId) {
+		String searchListQuery = "SELECT * FROM choice WHERE routineId=?";
+		Object[] param = new Object[] {routineId};
+		jdbcUtil.setSqlAndParameters(searchListQuery, param);
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();
+			Choice choice = new Choice();
+			
+			while (rs.next()) {
+				choice.setRoutineId(rs.getInt("routineId"));
+				choice.setExerciseId(rs.getInt("exerciseId"));
+				choice.setSequence(rs.getInt("sequence"));
+				choice.setRepetition(rs.getInt("repetition"));
+			}
+			return choice;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return null;
+	}
+
+	public Choice getChoiceByRoutineIdAndExerciseId(int routineId, int exerciseId) {
+		String searchQuery = "SELECT * FROM choice WHERE routineId=? and exerciseId=?";
+		Object[] param = new Object[] {routineId, exerciseId};
+		jdbcUtil.setSqlAndParameters(searchQuery, param);
+		Choice dto = null;
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();
+			if (rs.next()) {
+				dto = new Choice();
+				dto.setRoutineId(rs.getInt("routineId"));
+				dto.setExerciseId(rs.getInt("exerciseId"));
+				dto.setSequence(rs.getInt("sequence"));
+				dto.setRepetition(rs.getInt("repetition"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return dto;
+	}
+
+	public int insertChoice(Choice choice) {
+		int result = 0;
+		String insertQuery = "INSERT INTO "
+				+ "CHOICE (ROUTINEID, EXERCISEID, SEQUENCE, REPETITION) "
+				+ "VALUES (?, ?, ?, ?)";
+		Object[] param = new Object[] {choice.getRoutineId(), choice.getExerciseId(), choice.getSequence(), choice.getRepetition()};
+		jdbcUtil.setSqlAndParameters(insertQuery, param);
+		try {
+			result = jdbcUtil.executeUpdate();
+		} catch (Exception e) {
+			jdbcUtil.rollback();
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();
+		}
+		return result;
+	}
+
+	public int updateChoice(Choice choice, int routineId, int exerciseId, int sequence, int repetition) {
+		int result = 0;
+		String updateQuery = "UPDATE choice "
+				+ "SET routineId=?, exerciseId=?, sequence=?, repetition=? "
+				+ "WHERE routineId=? and exerciseId=? and sequence=? and repetition=?";
+		Object[] param = new Object[] {choice.getRoutineId(), choice.getExerciseId(), choice.getSequence(), choice.getRepetition(), 
+										routineId, exerciseId, sequence, repetition};
+		jdbcUtil.setSqlAndParameters(updateQuery, param);
+		try {
+			result = jdbcUtil.executeUpdate();
+		} catch (Exception e) {
+			jdbcUtil.rollback();
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();
+		}
+		return result;
+	}
+
+	public int deleteChoice(int routineId) {
+		int result = 0;
+		String deleteQuery = "DELETE FROM choice WHERE routineId=?";
+		Object[] param = new Object[] {routineId};
+		jdbcUtil.setSqlAndParameters(deleteQuery, param);
+		try {
+			result = jdbcUtil.executeUpdate();
+		} catch (Exception e) {
+			jdbcUtil.rollback();
+			e.printStackTrace();
+		} finally {
+			jdbcUtil.commit();
+			jdbcUtil.close();
+		}
+		return result;
+	}
 }
