46cd5f5f0ab22e9962755883703643ef05e22a3b
user <user@DESKTOP-4OALGTT>
2021-12-05 13:09:54 +0900

    Exercise 관련 수정

--- EASY_TO_WORKOUT/src/main/java/controller/routine/ListExerciseController.java
+++ EASY_TO_WORKOUT/src/main/java/controller/routine/ListExerciseController.java
@@ -39,7 +39,63 @@ public class ListExerciseController implements Controller {
 		request.setAttribute("exerciseList", exerciseList);
 		
 		request.setAttribute("routineName", request.getParameter("routineName"));
-		request.setAttribute("routinePart", request.getParameterValues("routinePart"));
+		
+		String[] parts = request.getParameterValues("routinePart");
+		String part = "";
+		int num = 0;
+	
+		if (parts != null) {
+			for (int i = 0; i < parts.length; i++) {
+				if (parts[i].equals("상체")) {
+					if (num == 0) {
+						part += "상체 ";
+					}
+					else {
+						part += ", 상체 ";
+					}
+					num += 1;
+				}
+				else if (parts[i].equals("하체")) {
+					if (num == 0) {
+						part += "하체 ";
+					}
+					else {
+						part += ", 하체 ";
+					}
+					num += 1;
+				}
+				else if (parts[i].equals("복부")) {
+					if (num == 0) {
+						part += "복부 ";
+					}
+					else {
+						part += ", 복부 ";
+					}
+					num += 1;
+				}
+				else if (parts[i].equals("어깨")) {
+					if (num == 0) {
+						part += "어깨 ";
+					}
+					else {
+						part += ", 어깨 ";
+					}
+					num += 1;
+				}
+				else {
+					if (num == 0) {
+						part += "전신 ";
+					}
+					else {
+						part += ", 전신 ";
+					}
+					num += 1;
+				}
+			}
+		}
+		request.setAttribute("routinePart", part);
+		
+		
 		request.setAttribute("routineTime", request.getParameter("routineTime"));
 		request.setAttribute("routineLevel", request.getParameter("routineLevel"));
 		request.setAttribute("routineType", request.getParameter("routineType"));
